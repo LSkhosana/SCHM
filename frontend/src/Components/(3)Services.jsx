ca5dd3ac5ed50d168f0f96a5ce3c4e0aba3b5fb3
Lesedi <skhosanalesedi888@gmail.com>
2024-10-14 21:01:30 +0200

    continue here

--- frontend/src/Components/(3)Services.jsx
+++ frontend/src/Components/(3)Services.jsx
@@ -1,8 +1,49 @@
 import React from 'react'
+import '../CSS/(3)Services.css'
+import chart from '../Assets/chart.png'
+import group from '../Assets/Group.png'
+import headphones from '../Assets/headphones.png'
+import network from '../Assets/network .png'
+import stock from '../Assets/stock.png'
+
 
 const Services = () => {
   return (
-    <div>Services</div>
+    <section className='service'>
+        <div className="service-container">
+            <div className='service-title'>
+                <h4>Services</h4>
+                <h2>Trade bigger and get paid faster.</h2>
+                <p>Lorem ipsum dolor sit amet, consectetur adipis elit</p>
+            </div>
+            <div className="service-grid">
+                <div className="box">
+
+                </div>
+                <div className="box">
+                <img src={chart} alt=''/>
+
+                </div>
+                <div className="box">
+                <img src={stock} alt=''/>
+
+                </div>
+                <div className="box">
+                <img src={group} alt=''/>
+
+                </div>
+                <div className="box">
+                <img src={headphones} alt=''/>
+
+                </div>
+                <div className="box">
+                <img src={network} alt=''/>
+
+                </div>
+            </div>
+        </div>
+
+    </section>
   )
 }
 
