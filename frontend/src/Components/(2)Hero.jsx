21f40307a411d1f02beaba8f8aac6176cb5c29fa
Lesedi <skhosanalesedi888@gmail.com>
2024-10-14 19:34:57 +0200

    hero complete

--- frontend/src/Components/(2)Hero.jsx
+++ frontend/src/Components/(2)Hero.jsx
@@ -1,9 +1,28 @@
 import React from 'react'
 import '../CSS/(2)Hero.css'
+import phone from '../Assets/Frame 445.png'
+// import stocks from '../Assets/stocks.png'
 
 const Hero = () => {
   return (
-    <div>Hero</div>
+    <section className='hero'>
+        <div className='hero-container'>
+            <div className='hero-text'>
+                <h1 className='hero-title'>live and <br/>on-demand training</h1>
+                <p className='hero-subtitle'>
+                    SCHMFOREX provides access to high-quality education,
+                    personalized training, and dedicated customer support, 
+                    ensuring that you have the tools needed to thrive in today’s ever-changing markets.
+                </p>
+                <button id='start'>Get started</button>
+            </div>
+            <div className='hero-img'>
+                <div className='phone'>
+                    <img src={phone} alt=''/>
+                </div>
+            </div>
+        </div>
+    </section>
   )
 }
 
