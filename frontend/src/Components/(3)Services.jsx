101e0d800afc9eab866ace0b3d537ee4b3a99151
Lesedi <skhosanalesedi888@gmail.com>
2024-10-15 10:27:40 +0200

    services complete

--- frontend/src/Components/(3)Services.jsx
+++ frontend/src/Components/(3)Services.jsx
@@ -17,27 +17,62 @@ const Services = () => {
                 <p>Lorem ipsum dolor sit amet, consectetur adipis elit</p>
             </div>
             <div className="service-grid">
-                <div className="box">
+                <div className="box" id='box1'>
 
                 </div>
-                <div className="box">
-                <img src={chart} alt=''/>
+                <div className="box" id='box2'>
+                  <div className='i'>
+                    <img src={chart} alt=''/>
+                  </div>
+                  <h4>Comprehensive Training</h4>
+                  <p>
+                    Learn at your own pace with access to a variety 
+                    of group and individual learning sessions
+                    tailored to your level.
+                  </p>
+                </div>
+                <div className="box" id='box3'>
+                  <div className='i'>
+                    <img src={stock} alt=''/>
+                  </div>                  
+                  <h4>Live Trading Sessions</h4>
+                  <p>
+                    Participate in regular live webinars led by seasoned mentors, 
+                    covering key market topics and providing practical,
+                    real-time insights.
+                  </p>
+                </div>
+                <div className="box" id='box4'>
+                  <div className='i'>
+                    <img src={group} alt=''/>
+                  </div>                  
+                  <h4>Flexible Learning</h4>
+                  <p>
+                  Choose between group sessions for collaborative learning or 
+                  personalized 1-on-1 mentorship for more focused attention.
+                  </p>
 
                 </div>
-                <div className="box">
-                <img src={stock} alt=''/>
+                <div className="box" id='box5'>
+                  <div className='i'>
+                    <img src={headphones} alt=''/>
+                  </div>
+                  <h4>Ongoing Support</h4>
+                  <p>
+                  Receive daily support from dedicated account managers and mentors, 
+                  keeping you informed with the latest market developments and strategies. 
+                  </p>
 
                 </div>
-                <div className="box">
-                <img src={group} alt=''/>
-
-                </div>
-                <div className="box">
-                <img src={headphones} alt=''/>
-
-                </div>
-                <div className="box">
-                <img src={network} alt=''/>
+                <div className="box" id='box6'>
+                  <div className='i'>
+                    <img src={network} alt=''/>
+                  </div>
+                  <h4>Community Engagement</h4>
+                  <p>
+                  Connect with other traders in our community, sharing experiences 
+                  and gaining valuable insights.
+                  </p>
 
                 </div>
             </div>
