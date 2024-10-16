733caf8b06d62b12f2bcfea87fdb7198184446aa
Lesedi <skhosanalesedi888@gmail.com>
2024-10-16 10:54:47 +0200

    values completed

--- frontend/src/Components/(6)Features.jsx
+++ frontend/src/Components/(6)Features.jsx
@@ -1,8 +1,45 @@
 import React from 'react'
-
+import '../CSS/(6)Features.css'
+import candle from '../Assets/candles.png'
+import market from '../Assets/market.png'
+import sys from '../Assets/Mask group-1.png'
+ 
 const Features = () => {
   return (
-    <div>Features</div>
+    <section className='features'>
+      <div className='features-container'>
+        <div className='features-head'>
+          <h2>Additional Services</h2>
+          <p>Lorem ipsum dolor sit amet. Qui consequatur sint 33 voluptatem officia et sint laboriosam sed ipsa sint ut voluptatum labore et possimus voluptas.</p>
+        </div>
+        <div className='feature-cards'>
+          <div className='feature-card'>
+            <p id='num'>01</p>
+            <div className='items'>
+              <img src={candle} alt=''/>
+              <h3>Daily Trading Rooms</h3>
+              <p>Join our live trading rooms, focused on key fundamental events, to stay ahead of market trends.</p>
+            </div>
+          </div>
+          <div className='feature-card'>
+          <p id='num'>02</p>
+            <div className='items'>
+              <img src={market} alt=''/>
+              <h3>Market Analysis & Insights</h3>
+              <p>Access high-quality market analysis to enhance your understanding and improve decision-making.</p>
+            </div>
+          </div>
+          <div className='feature-card'>
+          <p id='num'>03</p>
+            <div className='items'>
+              <img src={sys} alt=''/>
+              <h3>Tailored Trading Systems</h3>
+              <p>Work with our mentors to create personalized trading strategies that align with your financial goals.</p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
   )
 }
 
--- frontend/src/Components/(7)Values.jsx
+++ frontend/src/Components/(7)Values.jsx
@@ -1,8 +1,45 @@
 import React from 'react'
+import value from '../Assets/Frame 440.png'
+import '../CSS/(7)Values.css'
+
 
 const Values = () => {
   return (
-    <div>Values</div>
+    <section className='values'>
+      <div className='values-container'>
+        <div className='values-img'>
+          <img src={value} alt='values' />
+        </div>
+        <div className='values-text'>
+          <h4>Values</h4>
+          <h2>Our Core Values</h2>
+          <div className='para'>
+            <h5>Equality:</h5>
+            <p>
+            We believe in providing equal access to education and trading resources 
+            for individuals from all walks of life, across all experience levels. 
+            </p>
+          </div>
+
+          <div className='para'>
+            <h5>Transparency:</h5>
+            <p>
+            As a community-driven platform, we pride ourselves on honesty
+            and integrity, ensuring that our clients receive clear, trustworthy services.             
+            </p>
+          </div>
+
+          <div className='para'>
+            <h5>Trust</h5>
+            <p>
+            We foster long-term relationships with both individuals and institutions, 
+            creating a supportive community where members feel connected and valued.            
+            </p>
+          </div>
+          <button id='start2'>Learn More</button>
+        </div>
+      </div>
+    </section>
   )
 }
 
